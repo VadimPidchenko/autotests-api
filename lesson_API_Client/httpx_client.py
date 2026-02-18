6d88eccb12df65f7e7c1ccbfa6b9658132508b74
VadimPidchenko <pidchenko.vadim@icloud.com>
2026-02-18 21:34:46 +0300

    Знакомство с API клиентами

---
+++ lesson_API_Client/httpx_client.py
@@ -0,0 +1,26 @@
+import httpx
+
+login_payload = {
+    "email": "user@example.com",
+    "password": "string"
+}
+
+login_response = httpx.post("http://localhost:8000/api/v1/authentication/login", json=login_payload)
+login_response_data = login_response.json()
+
+print("Login status code:", login_response.status_code)
+print("Login data:", login_response_data)
+
+client = httpx.Client(
+    base_url="http://localhost:8000",
+    headers={
+        "Authorization": f"Bearer {login_response_data["token"]["accessToken"]}"
+    },
+    timeout=10
+)
+
+get_user_me_response = client.get("/api/v1/users/me")
+get_user_me_response_data = get_user_me_response.json()
+
+print("Get user status code:", get_user_me_response.status_code)
+print("User data:", get_user_me_response_data)
