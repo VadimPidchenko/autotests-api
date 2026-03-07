b57435b92faa1a04db3aecac4db64b7117eed5db
VadimPidchenko <pidchenko.vadim@icloud.com>
2026-03-07 23:08:30 +0300

    Знакомство с Faker. Практика работы с генерацией случайных данных

--- api_client_create_exercise.py
+++ api_client_create_exercise.py
@@ -11,17 +11,10 @@ from clients.private_http_builder import (
 )
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema
-from tools.fakers import fake
 
 public_user_client = get_public_users_client()
 
-create_user_request = CreateUserRequestSchema(
-    email= fake.email(),
-    password="string",
-    last_name="str",
-    first_name="str",
-    middle_name="str"
-)
+create_user_request = CreateUserRequestSchema()
 create_user_response = public_user_client.create_user(create_user_request)
 
 
@@ -37,8 +30,6 @@ exercise_client = get_exercise_client(authentication_user)
 
 
 create_file_request = CreateFileRequestSchema(
-    filename="image.png",
-    directory="courses",
     upload_file="testdata/files/image.png"
 )
 create_file_response = files_client.create_file(create_file_request)
@@ -46,11 +37,6 @@ print("Create file data:", create_file_response)
 
 
 create_course_request = CreateCourseRequestSchema(
-    title="Python",
-    max_score=100,
-    min_score=10,
-    description="Python API course",
-    estimated_time="2 weeks",
     preview_file_id=create_file_response.file.id,
     created_by_user_id=create_user_response.user.id,
 )
@@ -59,13 +45,7 @@ print("Create course data:", create_course_response)
 
 
 create_exercise_request = CreateExercisesRequestSchema(
-    title= "Exercise 1",
-    course_id= create_course_response.course.id,
-    max_score= 5,
-    min_score= 2,
-    order_index= 1,
-    description= "description exercise",
-    estimated_time= "30 minutes"
+    course_id= create_course_response.course.id
 )
 create_exercise_response = exercise_client.create_exercise(create_exercise_request)
 print("Create exercise data:", create_exercise_response)
