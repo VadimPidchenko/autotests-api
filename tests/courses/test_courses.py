a0caedde3a674bdcaaa51d3ea539bc5b58e23edb
VadimPidchenko <pidchenko.vadim@icloud.com>
2026-04-05 13:10:38 +0300

    Практикуемся в написании API автотестов. Часть 2. Практика написания API автотестов для работы с курсами

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -8,13 +8,17 @@ from clients.courses.courses_schema import (
     UpdateCourseResponseSchema,
     GetCoursesQuerySchema,
     GetCoursesResponseSchema,
+    CreateCourseRequestSchema,
+    CreateCourseResponseSchema,
 )
 from fixtures.courses import CoursesFixture
+from fixtures.files import FileFixture
 from fixtures.users import UserFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.courses import (
     assert_update_course_response,
     assert_get_courses_response,
+    assert_create_course_response,
 )
 from tools.assertions.schema import validate_json_schema
 
@@ -26,7 +30,9 @@ class TestCourses:
         self, courses_client: CoursesClient, function_create_course: CoursesFixture
     ):
         request = UpdateCourseRequestSchema()
-        response = courses_client.update_course_api(function_create_course.response.course.id, request)
+        response = courses_client.update_course_api(
+            function_create_course.response.course.id, request
+        )
         response_data = UpdateCourseResponseSchema.model_validate_json(response.text)
 
         assert_status_code(response.status_code, HTTPStatus.OK)
@@ -35,16 +41,35 @@ class TestCourses:
         validate_json_schema(response.json(), response_data.model_json_schema())
 
     def test_get_courses(
-            self,
-            courses_client: CoursesClient,
-            function_create_course: CoursesFixture,
-            function_create_user: UserFixture
+        self,
+        courses_client: CoursesClient,
+        function_create_course: CoursesFixture,
+        function_create_user: UserFixture,
     ):
-        query = GetCoursesQuerySchema(userId = function_create_user.response.user.id)
+        query = GetCoursesQuerySchema(user_id=function_create_user.response.user.id)
         response = courses_client.get_courses_api(query)
         response_data = GetCoursesResponseSchema.model_validate_json(response.text)
 
         assert_status_code(response.status_code, HTTPStatus.OK)
         assert_get_courses_response(response_data, [function_create_course.response])
+
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_create_course(
+        self,
+        courses_client: CoursesClient,
+        function_create_file: FileFixture,
+        function_create_user: UserFixture,
+    ):
+        request = CreateCourseRequestSchema(
+            preview_file_id=function_create_file.response.file.id,
+            created_by_user_id=function_create_user.response.user.id,
+        )
+
+        response = courses_client.create_course_api(request)
+        response_data = CreateCourseResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_create_course_response(request, response_data)
 
         validate_json_schema(response.json(), response_data.model_json_schema())
