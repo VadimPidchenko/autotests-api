0f9e71f1c77b306ab463ebbbc726a3f8bc449855
VadimPidchenko <pidchenko.vadim@icloud.com>
2026-05-02 14:22:59 +0300

    Добавляем информацию о переменных окружения в Allure отчет. Практика работы с переменными окружения в Allure

--- tools/allure/environment.py
+++ tools/allure/environment.py
@@ -1,8 +1,13 @@
+import platform
+import sys
+
 from config import settings
 
 
 def create_allure_environment_file():
     item = [f"{key}={value}" for key, value in settings.model_dump().items()]
+    item.append(f"os_info={platform.system()}, {platform.release()}")
+    item.append(f"python_version={sys.version}")
 
     properties = "\n".join(item)
 
