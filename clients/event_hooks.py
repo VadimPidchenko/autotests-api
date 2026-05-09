0807261ef6b9338bfe88d9432bada4f0b572eca1
VadimPidchenko <pidchenko.vadim@icloud.com>
2026-05-09 22:12:48 +0300

    Added run autotests on CI

--- clients/event_hooks.py
+++ clients/event_hooks.py
@@ -1,6 +1,5 @@
 import allure
-from httpx import Request
-from websockets import Response
+from httpx import Request, Response
 
 from tools.http.curl import make_curl_from_request
 from tools.logger import get_logger
